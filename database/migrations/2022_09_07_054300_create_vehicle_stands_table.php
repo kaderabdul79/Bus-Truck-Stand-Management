b7b568a8b90745e74a2c164154d3b2926bd04ea7
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-06-16 08:38:45 +0600

    define db and created migration

---
+++ database/migrations/2022_09_07_054300_create_vehicle_stands_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_stands', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ParkingNumber')->nullable();
+            $table->string('VehicleType');
+            $table->string('DriverName');
+            $table->bigInteger('Drivermobilenumber');
+            $table->bigInteger('Driverlicensenumber');
+            $table->bigInteger('Vehicleregistrationnumber');
+            $table->string('Remark')->nullable();
+            $table->integer('Charge')->nullable();
+            $table->boolean('Status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_stands');
+    }
+};
